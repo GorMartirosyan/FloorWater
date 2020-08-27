5c4790ce64220f0b7e20dceb409cd326124a579f
Gor <user1@user1s-MacBook-Pro.local>
2020-08-28 03:07:45 +0400

    Commit

--- FloorWater/ViewController.swift
+++ FloorWater/ViewController.swift
@@ -18,69 +18,69 @@ class ViewController: UIViewController,ARSCNViewDelegate {
     
     @IBOutlet weak var sceneView: ARSCNView!
     
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            configuration.planeDetection = .horizontal
-            configuration.environmentTexturing = .automatic
-            
-            sceneView.automaticallyUpdatesLighting = true
-            sceneView.session.run(configuration)
-            sceneView.delegate = self
-            
-            waterImageView.loadGif(name: "water")
-            flowerImageView.loadGif(name: "flowers")
-            waterImageView.addSubview(flowerImageView)
-        }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configuration.planeDetection = .horizontal
+        configuration.environmentTexturing = .automatic
         
-        func createWater(with planeAnchor : ARPlaneAnchor) -> SCNNode{
-            
-            DispatchQueue.main.sync {
-                waterImageView.frame = CGRect(x: CGFloat(planeAnchor.center.x), y: CGFloat(planeAnchor.center.y), width: self.view.frame.width / 2, height: self.view.frame.height / 2)
-                flowerImageView.frame.size = CGSize(width: waterImageView.frame.width * 2/3, height: waterImageView.frame.height / 2)
-                flowerImageView.center.x = waterImageView.center.x
-                flowerImageView.center.y = waterImageView.center.y
-            }
-            
-            let waterNode = SCNNode(geometry: SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z)))
-            waterNode.geometry?.firstMaterial?.diffuse.contents = waterImageView
-            waterNode.geometry?.firstMaterial?.isDoubleSided = true
-            waterNode.geometry?.firstMaterial?.lightingModel = .physicallyBased
-            waterNode.geometry?.firstMaterial?.roughness.intensity = 1.0
-            waterNode.geometry?.firstMaterial?.specular.contents = 1.0
-            waterNode.position = SCNVector3(planeAnchor.center.x,planeAnchor.center.y,planeAnchor.center.z)
-            waterNode.eulerAngles = SCNVector3(90.degreesToRadians,0,0)
-            return waterNode
-        }
-
+        sceneView.automaticallyUpdatesLighting = true
+        sceneView.session.run(configuration)
+        sceneView.delegate = self
         
-        func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-            guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
-            let waterNode = createWater(with: planeAnchor)
-            node.addChildNode(waterNode)
-        }
-        
-        func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-            guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
-            node.enumerateChildNodes { (childNode, _) in
-                childNode.removeFromParentNode()
-            }
-            let waterNode = createWater(with: planeAnchor)
-            node.addChildNode(waterNode)
-        }
-        
-        func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
-            
-            guard let _ = anchor as? ARPlaneAnchor else {return}
-            node.enumerateChildNodes { (childNode, _) in
-                childNode.removeFromParentNode()
-            }
-        }
-        
-        
+        waterImageView.loadGif(name: "water")
+        flowerImageView.loadGif(name: "flowers")
+        waterImageView.addSubview(flowerImageView)
     }
-
-    extension Int {
+    
+    func createWater(with planeAnchor : ARPlaneAnchor) -> SCNNode{
         
-        var degreesToRadians : Double {return Double(self) * .pi/180}
+        DispatchQueue.main.sync {
+            waterImageView.frame = CGRect(x: CGFloat(planeAnchor.center.x), y: CGFloat(planeAnchor.center.y), width: self.view.frame.width / 2, height: self.view.frame.height / 2)
+            flowerImageView.frame.size = CGSize(width: waterImageView.frame.width * 2/3, height: waterImageView.frame.height / 2)
+            flowerImageView.center.x = waterImageView.center.x
+            flowerImageView.center.y = waterImageView.center.y
+        }
+        
+        let waterNode = SCNNode(geometry: SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z)))
+        waterNode.geometry?.firstMaterial?.diffuse.contents = waterImageView
+        waterNode.geometry?.firstMaterial?.isDoubleSided = true
+        waterNode.geometry?.firstMaterial?.lightingModel = .physicallyBased
+        waterNode.geometry?.firstMaterial?.roughness.intensity = 1.0
+        waterNode.geometry?.firstMaterial?.specular.contents = 1.0
+        waterNode.position = SCNVector3(planeAnchor.center.x,planeAnchor.center.y,planeAnchor.center.z)
+        waterNode.eulerAngles = SCNVector3(90.degreesToRadians,0,0)
+        return waterNode
     }
+    
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        let waterNode = createWater(with: planeAnchor)
+        node.addChildNode(waterNode)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        node.enumerateChildNodes { (childNode, _) in
+            childNode.removeFromParentNode()
+        }
+        let waterNode = createWater(with: planeAnchor)
+        node.addChildNode(waterNode)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        
+        guard let _ = anchor as? ARPlaneAnchor else {return}
+        node.enumerateChildNodes { (childNode, _) in
+            childNode.removeFromParentNode()
+        }
+    }
+    
+    
+}
+
+extension Int {
+    
+    var degreesToRadians : Double {return Double(self) * .pi/180}
+}
 
